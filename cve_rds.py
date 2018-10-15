0e33ab2e47e189758cbb87b84a5c8c897b0c493c
root <root@ip-172-31-93-96.ec2.internal>
2018-10-15 10:04:19 +0000

    latest cve_rds

--- cve_rds.py
+++ cve_rds.py
@@ -1,22 +1,24 @@
-
 import boto3
 import numpy as np
+import requests
+
+from jira import JIRA
 from ares import CVESearch
 from prettytable import PrettyTable
 import textwrap
 from slackclient import SlackClient
 
-table = PrettyTable(header_style='upper',field_names=["Database Name", "Engine Name", "Engine version", "Region",'Availability Zone',"CVE ID","Score","Impact","Summary"])
-impactTable = PrettyTable(header_style='upper',field_names=["Database Name", "Engine Name", "Engine version", "Region",'Availability Zone',"CVE ID","Score","Impact","Summary"])
+table = PrettyTable(header_style='upper',field_names=["Database Name", "Engine Name", "Engine version", "Region",'Availability Zone',"CVE ID","Score","Impact"])
+impactTable = PrettyTable(header_style='upper',field_names=["Database Name", "Engine Name", "Engine version", "Region",'Availability Zone',"CVE ID","Score","Impact"])
+securityAdminsession = boto3.Session(profile_name='securityAdmin' , region_name='us-west-2')
 cve = CVESearch()
-ec2 = boto3.client('ec2')
+ec2 = securityAdminsession.client('ec2')
 regions = ec2.describe_regions()
 for region in regions["Regions"]:
-   source = boto3.client('rds',region_name=region["RegionName"])
+   source = securityAdminsession.client('rds',region_name=region["RegionName"])
    instances = source.describe_db_instances()
    db_instanceCount=np.array(instances.get('DBInstances')).size
    for dbInstance in instances.get('DBInstances'):
-      
       engine_version = dbInstance.get('EngineVersion')
       engine = dbInstance.get('Engine')
       dbName = dbInstance.get('MasterUsername')
@@ -31,7 +33,7 @@ for region in regions["Regions"]:
 
       print("CVE for: "+key)
       output_dict = [x for x in dbvendor if x['vulnerable_configuration'].count(key)>0]
-      
+      print("Account ID: ",dbInstance.get('DBInstanceArn').split(':')[4])
       print("Database Name: ",dbName)
       print("Engine Name: ",engine)
       print("Engine version: ",engine_version )
@@ -50,26 +52,34 @@ for region in regions["Regions"]:
       CVEID=str(output_dict[0]["id"])
       CVSS=str(output_dict[0]["cvss"])
       summary=str(output_dict[0]["summary"])
-
-      if(float(output_dict[0]["cvss"]) >= 7.0):
+      
+      if(float(output_dict[0]["cvss"]) >= 6.6):
          impact="high"
-      elif(float(output_dict[0]["cvss"]) >= 4.1 and float(output_dict[0]["cvss"]) <= 6.0):
+      elif(float(output_dict[0]["cvss"]) >= 4.1 and float(output_dict[0]["cvss"]) <= 6.5):
          impact="medium"
       else:
          impact="low"
 
-      table.add_row([databaseName,engineName,engineVersion,regionName,availabilityZone,CVEID,CVSS,impact,textwrap.fill(summary,30)])
+      table.add_row([databaseName,engineName,engineVersion,regionName,availabilityZone,CVEID,CVSS,impact])
 
       if( impact == "low" ): 
-         impactTable.add_row([databaseName,engineName,engineVersion,regionName,availabilityZone,CVEID,CVSS,impact,textwrap.fill(summary,30)])
-         
+         impactTable.add_row([databaseName,engineName,engineVersion,regionName,availabilityZone,CVEID,CVSS,impact])
+
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+jira = JIRA({'server':'https://sunnysimhadri07.atlassian.net','verify':False}, basic_auth=('sunny.simhadri@gmail.com', 'q6EcEncBLjKGmlWmwSII39CB'))
+new_issue = jira.create_issue(project='10000', summary='New issue from jira-python',description=table.get_string(), issuetype={'name': 'Story'}) 
+
+
+
 print(table)
 if(impactTable.rowcount > 0):
-   slack_token = "xoxb-452967264370-452968226034-0yQIcwAV30tI9wqk1AEZMBv1"
+   slack_token = "xoxb-447000172100-447001621796-1LmPxW4EvICx4gMuRJo254hv"
    sc = SlackClient(slack_token)
    sc.api_call(
         "chat.postMessage",
         channel="#general",
-        text=impactTable,
+        #text='https://sunnysimhadri07.atlassian.net/browse/'+str(new_issue) ,
+        
+        text=table.get_string() ,
         type= "message",
    )
